d3f981bf6f912df1efda6e1e7b6049d562adad17
egdegd <egd3700@mail.ru>
2019-05-11 17:21:43 +0300

    add model.py

--- task06/model.py
+++ task06/model.py
@@ -4,7 +4,60 @@ import abc
 
 class Scope:
     def __init__(self, parent=None):
-        raise NotImplementedError
+        self.parent = parent
+        self.dict = {}
+
+    def __getitem__(self, key):
+        if key in self.dict:
+            return self.dict[key]
+        if self.parent:
+            return self.parent[key]
+        raise KeyError(key)
+
+    def __setitem__(self, key, value):
+        self.dict[key] = value
+
+
+class ASTNodeVisitor(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def visit_number(self, number):
+        pass
+
+    @abc.abstractmethod
+    def visit_function(self, function):
+        pass
+
+    @abc.abstractmethod
+    def visit_function_definition(self, func_def):
+        pass
+
+    @abc.abstractmethod
+    def visit_conditional(self, conditional):
+        pass
+
+    @abc.abstractmethod
+    def visit_print(self, print):
+        pass
+
+    @abc.abstractmethod
+    def visit_read(self, read):
+        pass
+
+    @abc.abstractmethod
+    def visit_function_call(self, func_call):
+        pass
+
+    @abc.abstractmethod
+    def visit_reference(self, reference):
+        pass
+
+    @abc.abstractmethod
+    def visit_binary_operation(self, bin_op):
+        pass
+
+    @abc.abstractmethod
+    def visit_unary_operation(self, un_op):
+        pass
 
 
 class ASTNode(metaclass=abc.ABCMeta):
