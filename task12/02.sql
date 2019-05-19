64b1246a2094e8a1bf0ca5a3dac6a89a13d0602b
egdegd <egd3700@mail.ru>
2019-05-19 13:31:45 +0300

    task12

--- task12/02.sql
+++ task12/02.sql
@@ -1,4 +1,6 @@
 -- Выведите название страны с максимальным уровнем грамотности по последним
 -- данным, которые доступны для страны. В выводе: название страны и уровень
 -- грамотности, именно в таком порядке и без лишних полей. (0,75 баллов)
-SELECT * FROM Country;
+SELECT Name FROM Country
+WHERE Code = (SELECT CountryCode FROM LiteracyRate
+WHERE Rate = (SELECT MAX(Rate) FROM LiteracyRate));
